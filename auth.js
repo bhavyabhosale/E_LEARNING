6ffd6f7c5bf051d47d5583935d7cd0d3c7d5f7a6
Bhavya Bhosale <123302596+bhavyabhosale@users.noreply.github.com>
2024-08-09 21:59:08 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
     apiKey: "",
-    authDomain: "my-project-d1d8a.firebaseapp.com",
-    projectId: "my-project-d1d8a",
-    storageBucket: "my-project-d1d8a.appspot.com",
-    messagingSenderId: "1041578841806",
-    appId: "1:1041578841806:web:cf6321d5ced8578a97629f",
-    measurementId: "G-7FTSQPQ7J4"
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
